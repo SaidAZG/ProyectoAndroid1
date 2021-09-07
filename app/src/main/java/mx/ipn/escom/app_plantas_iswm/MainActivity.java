6d415677b9890dc39f9bd5fb4814dc05fd4d9a5a
SaidAZG <saidazg_lego@hotmail.com>
2021-09-06 22:43:10 -0500

    Creación del Proyecto de Android
    Primer Commit con la versión base del sistema

---
+++ app/src/main/java/mx/ipn/escom/app_plantas_iswm/MainActivity.java
@@ -0,0 +1,14 @@
+package mx.ipn.escom.app_plantas_iswm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

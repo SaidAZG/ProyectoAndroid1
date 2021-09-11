fc79dbdb17609dd86f3f0c2f4c2afef3a0c90620
JeanSer85 <jeansernac96@gmail.com>
2021-09-11 12:53:39 -0500

    Develop_Jean

--- app/src/main/java/mx/ipn/escom/app_plantas_iswm/MainActivity.java
+++ app/src/main/java/mx/ipn/escom/app_plantas_iswm/MainActivity.java
@@ -1,14 +1,15 @@
 package mx.ipn.escom.app_plantas_iswm;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+       // setContentView(R.layout.p5a_editplant);
+        setContentView(R.layout.p6_addplants);
     }
 }
